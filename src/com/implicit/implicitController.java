99d84158824c5c96d202e81eb71a906cdf292c88
ManikaAgarwal <manika615@gmail.com>
2015-08-28 00:43:40 +0800

    calling getInput()

--- src/com/implicit/implicitController.java
+++ src/com/implicit/implicitController.java
@@ -2,6 +2,9 @@ package com.implicit;
 
 public class implicitController {
     public void getInput() {
+        Input in = new Input();
+        Lines rawData = new Lines();
         
+        in.getInput(rawData);
     }
 }
