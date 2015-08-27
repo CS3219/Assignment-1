7b9c9acfe3e7e568a79dcc88725b28b07387f1ed
ManikaAgarwal <manika615@gmail.com>
2015-08-28 00:44:32 +0800

    reading in words to ignore

--- src/com/implicit/Input.java
+++ src/com/implicit/Input.java
@@ -1,11 +1,22 @@
 package com.implicit;
 
+import java.util.Scanner;
+
 /**
- * Gets input from user and implicitly calls LineStorage class.
+ * Gets input from user.
  * @author Manika
  *
  */
 
 public class Input {
-
+    public void getInput(Lines rawData) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter words to ignore separated by spaces: ");
+        scanner.nextLine();
+        String wordsToIgnore = scanner.nextLine();
+        scanner.close();
+        rawData.insertWordsToIgnore(wordsToIgnore);
+        
+    }
 }
