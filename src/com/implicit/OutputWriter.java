c096fa8a927c6e6c47797cd3bcd23068de14284a
ManikaAgarwal <manika615@gmail.com>
2015-08-28 18:57:46 +0800

    wrote print fn

--- src/com/implicit/OutputWriter.java
+++ src/com/implicit/OutputWriter.java
@@ -1,8 +1,14 @@
 package com.implicit;
 
+import java.util.ArrayList;
+
 public class OutputWriter {
     
-    public void print() {
+    public void print(LineStorage lines) {
+        ArrayList<String> titles = lines.getAllTitles();
         
+        for(int i = 0; i < titles.size(); i++) {   
+            System.out.println(titles.get(i));
+        } 
     }
 }
