efe4b63c45f1df931cc88a598b8c4d346f372b57
Vishnu <vishki.priya@gmail.com>
2015-08-29 23:08:15 +0800

    Added circular shift filter

---
+++ src/com/pipe_and_filter/CircularShift.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class CircularShift {
+	
+	private ArrayList<String> originalList;
+	private ArrayList<String> shiftedList;
+	private ArrayList<String> wordsIgnore;
+
+	
+	public CircularShift(ArrayList<String> titles, ArrayList<String> words) {
+		this.originalList = titles;
+		shiftedList = new ArrayList<String>();
+		this.wordsIgnore = words;
+	}
+	
+	private void shift() {
+		for(int i=0; i<originalList.size();i++) {
+			String title = originalList.get(i);
+			shiftedList.add(title);
+			lineShift(title);
+		}
+	}
+	
+	private void lineShift(String line) {
+		String[] words = line.split(" ");
+		for(int i=0;i<words.length-1;i++) {
+			int index = line.indexOf(" ");
+			String firstWord = line.substring(0,index);
+			String remainingLine = line.substring(index+1, line.length());
+			String newLine = remainingLine + " " + firstWord;
+			shiftedList.add(newLine);
+			line = newLine;
+		}
+	}
+	
+	public void passData() {
+		shift();
+		Pipe2 pipe = new Pipe2(shiftedList,wordsIgnore);
+		pipe.passData();
+	}
+	
+}
