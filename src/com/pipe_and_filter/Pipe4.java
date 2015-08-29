80ee868e63e621803f97651215898a159357f7ff
Vishnu <vishki.priya@gmail.com>
2015-08-29 23:12:45 +0800

    Added the fourth pipe

---
+++ src/com/pipe_and_filter/Pipe4.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Pipe4 {
+	
+	private ArrayList<String> list;
+	private ArrayList<String> wordsIgnore;
+	
+	public Pipe4(ArrayList<String> list, ArrayList<String> ignore ) {
+		this.list = list;
+		this.wordsIgnore = ignore;
+	}
+	
+	public void passData() {
+		Output output = new Output(list, wordsIgnore);
+		output.passData();
+	}
+
+}
