c4e972a71848b86f21a9a36d78370e3b1d5dd8fb
Vishnu <vishki.priya@gmail.com>
2015-08-29 23:11:54 +0800

    Added the third pipe

---
+++ src/com/pipe_and_filter/Pipe3.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Pipe3 {
+	
+	private ArrayList<String> list;
+	private ArrayList<String> wordsIgnore;
+	
+	public Pipe3(ArrayList<String> list, ArrayList<String> ignore) {
+		this.list = list;
+		this.wordsIgnore = ignore;
+	}
+	
+	public void passData() {
+		Alphabetize alphabetizer = new Alphabetize(list, wordsIgnore);
+		alphabetizer.passData();
+	}
+
+}
