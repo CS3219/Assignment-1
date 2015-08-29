13bbae12a010595597e3113d8d413a34b8783529
Vishnu <vishki.priya@gmail.com>
2015-08-29 23:11:25 +0800

    Added filter to remove lines beginning with ignored words

---
+++ src/com/pipe_and_filter/RemoveIgnored.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class RemoveIgnored {
+	
+	private ArrayList<String> list;
+	private ArrayList<String> wordsRemove;
+	
+	public RemoveIgnored(ArrayList<String> list, ArrayList<String> words) {
+		this.list = list;
+		this.wordsRemove = words;
+	}
+	
+	private void removeLines() {
+		for(int i=0; i<list.size(); i++) {
+			String line = list.get(i);
+			int index = line.indexOf(" ");
+			String firstWord;
+			if(index == -1) {
+				firstWord = line;
+			} else {
+			firstWord = line.substring(0, index);
+			}
+			checkIgnore(i, firstWord);
+		}
+	}
+	
+	private void checkIgnore(int index, String word) {
+		for(int i=0; i<wordsRemove.size(); i++ ){
+			String ignore = wordsRemove.get(i);
+			if(word.equalsIgnoreCase(ignore)) {
+				list.remove(index);
+			}
+		}
+	}
+	
+	public void passData() {
+		removeLines();
+		Pipe3 pipe = new Pipe3(list, wordsRemove);
+		pipe.passData();
+	}
+}
