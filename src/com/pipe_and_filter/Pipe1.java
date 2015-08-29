c3e4ba7b6fc17b70a7832ea0989c25fed00240e6
Vishnu <vishki.priya@gmail.com>
2015-08-29 23:07:36 +0800

    Added first pipe

---
+++ src/com/pipe_and_filter/Pipe1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Pipe1 {
+	
+	private ArrayList<String> titles;
+	private ArrayList<String> wordsIgnore;
+	
+	public Pipe1(ArrayList<String> inputList, ArrayList<String> words) {
+		this.titles = inputList;
+		this.wordsIgnore = words;
+	}
+	
+	public void passData() {
+		CircularShift circularshift = new CircularShift(titles, wordsIgnore);
+		circularshift.passData();
+	}
+}
